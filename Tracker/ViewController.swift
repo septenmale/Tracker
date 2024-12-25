71c6ce8717330ca67ff1fb2a903707a04da6b7ec
Viktor <zavhorodnii.viktor@icloud.com>
2024-12-25 11:49:46 +0100

    Initial Commit

---
+++ Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tracker
+//
+//  Created by Viktor on 25/12/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

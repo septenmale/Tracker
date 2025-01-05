e61c993339ad52ae8fe4ce506128a67cbc6a25a2
Viktor <zavhorodnii.viktor@icloud.com>
2025-01-05 10:41:07 +0100

    add collectionView with stubs

---
+++ Tracker/Controllers/Trackers/CollectionViewDataSource.swift
@@ -0,0 +1,7 @@
+//
+//  CollectionViewDataSource.swift
+//  Tracker
+//
+//  Created by Viktor on 05/01/2025.
+//
+
---
+++ Tracker/Controllers/Trackers/TrackersCell.swift
@@ -0,0 +1,33 @@
+//
+//  TrackersCell.swift
+//  Tracker
+//
+//  Created by Viktor on 05/01/2025.
+//
+
+import UIKit
+
+final class TrackersCell: UICollectionViewCell {
+    
+    static let reuseIdentifier = "TrackersCell"
+    
+    let titleLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(titleLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            titleLabel.topAnchor.constraint(equalTo: topAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}

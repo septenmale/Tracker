20b27fa62e61739cf5ae425b0ef85c82502379a9
Viktor Zavhorodnii <septenmale@Viktors-Mac-mini.local>
2025-02-09 08:50:27 +0100

    Add NSManagedObjectClasses to track

---
+++ Tracker/CoreData/NSManagedObjectClasses/TrackerModel/TrackerCoreData+CoreDataProperties.swift
@@ -0,0 +1,49 @@
+//
+//  TrackerCoreData+CoreDataProperties.swift
+//  Tracker
+//
+//  Created by Viktor Zavhorodnii on 09/02/2025.
+//
+//
+
+import Foundation
+import CoreData
+import UIKit
+
+
+extension TrackerCoreData {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<TrackerCoreData> {
+        return NSFetchRequest<TrackerCoreData>(entityName: "TrackerCoreData")
+    }
+
+    @NSManaged public var color: UIColor?
+    @NSManaged public var emoji: String?
+    @NSManaged public var id: UUID?
+    @NSManaged public var schedule: NSObject?
+    @NSManaged public var title: String?
+    @NSManaged public var category: TrackerCategoryCoreData?
+    @NSManaged public var record: NSSet?
+
+}
+
+// MARK: Generated accessors for record
+extension TrackerCoreData {
+
+    @objc(addRecordObject:)
+    @NSManaged public func addToRecord(_ value: TrackerRecordCoreData)
+
+    @objc(removeRecordObject:)
+    @NSManaged public func removeFromRecord(_ value: TrackerRecordCoreData)
+
+    @objc(addRecord:)
+    @NSManaged public func addToRecord(_ values: NSSet)
+
+    @objc(removeRecord:)
+    @NSManaged public func removeFromRecord(_ values: NSSet)
+
+}
+
+extension TrackerCoreData : Identifiable {
+
+}
